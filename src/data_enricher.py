92c931c859d42bb11aa13cee79743f8be69f0012
HerrKurz <aleksander.kurz@gmail.com>
2023-07-07 16:10:44 +0200

    reformatting

--- src/data_enricher.py
+++ src/data_enricher.py
@@ -1,22 +1,25 @@
 """
 Enriches the data by handling additional extractions and data transformations.
 """
-from config import FIELDS, TEAM_FIELDS
 import itertools
 import json
+
 import mwclient
 import pandas as pd
 import requests
-from src.data_extractor import GetData
-from src.utils import append_id, append_country_to_player, append_player_info, clean_countries_list, clean_json, \
-     convert_to_json, append_team_info, adjust_dict_values
 from tqdm import tqdm
+
+from config import FIELDS, TEAM_FIELDS
+from src.data_extractor import GetData
+from src.utils import (adjust_dict_values, append_country_to_player, append_id,
+                       append_player_info, append_team_info,
+                       clean_countries_list, clean_json, convert_to_json)
 
 
 class DataEnricher:
     def __init__(self, data):
-        self.lol_site = mwclient.Site('lol.fandom.com', path='/')
-        self.wiki_site = mwclient.Site('en.wikipedia.org')
+        self.lol_site = mwclient.Site("lol.fandom.com", path="/")
+        self.wiki_site = mwclient.Site("en.wikipedia.org")
         self.players = self.extract_players()
         self.teams = self.extract_teams()
         self.complementary_dict = self.transform_player_data(data, self.players)
@@ -52,13 +55,19 @@ class DataEnricher:
             list: A list of player data.
         """
         player_list = []
-        for players_batch in tqdm(range(0, 17000, 500), colour='CYAN', desc=f'Extracting player data from Leaguepedia'):
-            response = self.lol_site.api('cargoquery',
-                                         limit='max',
-                                         tables="Players",
-                                         offset=players_batch,
-                                         fields=FIELDS,
-                                         format="json")
+        for players_batch in tqdm(
+            range(0, 17000, 500),
+            colour="CYAN",
+            desc=f"Extracting player data from Leaguepedia",
+        ):
+            response = self.lol_site.api(
+                "cargoquery",
+                limit="max",
+                tables="Players",
+                offset=players_batch,
+                fields=FIELDS,
+                format="json",
+            )
             try:
                 parsed = json.loads(json.dumps(response["cargoquery"]))
                 player_list.append(parsed)
@@ -76,13 +85,19 @@ class DataEnricher:
             list: A list of team data.
         """
         team_list = []
-        for team_batch in tqdm(range(0, 3500, 500), colour='CYAN', desc=f'Extracting team data from Leaguepedia'):
-            response = self.lol_site.api('cargoquery',
-                                         limit='max',
-                                         tables="Teams",
-                                         offset=team_batch,
-                                         fields=TEAM_FIELDS,
-                                         format="json")
+        for team_batch in tqdm(
+            range(0, 3500, 500),
+            colour="CYAN",
+            desc=f"Extracting team data from Leaguepedia",
+        ):
+            response = self.lol_site.api(
+                "cargoquery",
+                limit="max",
+                tables="Teams",
+                offset=team_batch,
+                fields=TEAM_FIELDS,
+                format="json",
+            )
             try:
                 parsed = json.loads(json.dumps(response["cargoquery"]))
                 team_list.append(parsed)
@@ -103,7 +118,11 @@ class DataEnricher:
         Returns:
             _type_: A dictionary containing the transformed player data.
         """
-        return {player["ID"]: player for player in player_list if player.get("ID") in data.player_names}
+        return {
+            player["ID"]: player
+            for player in player_list
+            if player.get("ID") in data.player_names
+        }
 
     def transform_team_data(self, data: GetData, team_list: list):
         """Complements enriched team data form Leaguepedia with a team names list from Oracle's Elixir match data.
@@ -115,7 +134,11 @@ class DataEnricher:
         Returns:
             _type_: A dictionary containing the transformed team data.
         """
-        return {team["OverviewPage"]: team for team in team_list if team.get("OverviewPage") in data.team_names}
+        return {
+            team["OverviewPage"]: team
+            for team in team_list
+            if team.get("OverviewPage") in data.team_names
+        }
 
     def get_player_countries(self, enriched_dict: dict) -> list:
         """Takes a dictionary of enriched player data and returns the list
@@ -127,7 +150,14 @@ class DataEnricher:
         Returns:
             list: A list of unique countries associated with the players.
         """
-        player_countries_list = list(set([player_val.get("Country") for player, player_val in enriched_dict.items()]))
+        player_countries_list = list(
+            set(
+                [
+                    player_val.get("Country")
+                    for player, player_val in enriched_dict.items()
+                ]
+            )
+        )
         return clean_countries_list(player_countries_list)
 
     def get_country_coordinates(self) -> dict:
@@ -137,14 +167,21 @@ class DataEnricher:
             dict: A dictionary containing the country coordinates.
         """
         coords = {}
-        for country in tqdm(self.countries, colour='CYAN', desc=f'Extracting geographic coordinates from MediaWiki API'):
-            result = self.wiki_site.api('query', prop='coordinates', titles=country)
+        for country in tqdm(
+            self.countries,
+            colour="CYAN",
+            desc=f"Extracting geographic coordinates from MediaWiki API",
+        ):
+            result = self.wiki_site.api("query", prop="coordinates", titles=country)
             try:
-                for page in result['query']['pages'].values():
-                    if 'coordinates' in page:
-                        coords[country] = [page['coordinates'][0]['lon'], page['coordinates'][0]['lat']]
+                for page in result["query"]["pages"].values():
+                    if "coordinates" in page:
+                        coords[country] = [
+                            page["coordinates"][0]["lon"],
+                            page["coordinates"][0]["lat"],
+                        ]
                     else:
-                        self.countries_missing_coords.append(page['title'])
+                        self.countries_missing_coords.append(page["title"])
             except KeyError as e:
                 print(f"EXCEPTION:{e}, Country {country} not found")
         print(f"Countries without coords matching {self.countries_missing_coords}.")
@@ -157,12 +194,20 @@ class DataEnricher:
             dict: A dictionary containing the missing country coordinates.
         """
         missing_coords = {}
-        for country in tqdm(self.countries_missing_coords, colour='CYAN', desc=f'Filling missing geographic coordinates from REST Countries API'):
+        for country in tqdm(
+            self.countries_missing_coords,
+            colour="CYAN",
+            desc=f"Filling missing geographic coordinates from REST Countries API",
+        ):
             try:
-                response = requests.get(f"https://restcountries.com/v3.1/name/{country}")
+                response = requests.get(
+                    f"https://restcountries.com/v3.1/name/{country}"
+                )
                 response_json = response.json()[0]["latlng"]
                 response_json.reverse()
-                missing_coords[country] = [int(coordinate) for coordinate in response_json]
+                missing_coords[country] = [
+                    int(coordinate) for coordinate in response_json
+                ]
             except KeyError as e:
                 print(f"EXCEPTION:{e}, Country {country} not found.")
         adjust_dict_values(missing_coords)
@@ -188,19 +233,27 @@ class DataEnricher:
         """
         country_code = {}
         countries_without_code = []
-        for country in tqdm(self.countries, colour='CYAN', desc=f'Extracting country codes from REST Countries API'):
+        for country in tqdm(
+            self.countries,
+            colour="CYAN",
+            desc=f"Extracting country codes from REST Countries API",
+        ):
             try:
-                response = requests.get(f"https://restcountries.com/v3.1/name/{country}")
+                response = requests.get(
+                    f"https://restcountries.com/v3.1/name/{country}"
+                )
                 country_code[country] = response.json()[0]["ccn3"]
             except KeyError as e:
                 countries_without_code.append(country)
-                print(f"Error {e} for {countries_without_code}. Can't find matching country code.")
+                print(
+                    f"Error {e} for {countries_without_code}. Can't find matching country code."
+                )
                 continue
         adjust_dict_values(country_code)
         return country_code
 
     def append_country_codes(self, df_match: pd.DataFrame) -> pd.DataFrame:
-        """"Adds a new column with country codes in ISO 3166-1 numeric encoding system.
+        """ "Adds a new column with country codes in ISO 3166-1 numeric encoding system.
 
         Parameters:
             df_match (pd.DataFrame): The main DataFrame to be transformed.
